3dde68b964161f5c343544c60f73ac2dcc547139
Corey Beavers <coreybeavers1999@gmail.com>
2022-10-16 09:44:21 -0400

    Initial commit

---
+++ app/models/testing_database.rb
@@ -0,0 +1,2 @@
+class TestingDatabase < ApplicationRecord
+end
---
+++ test/models/testing_database_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class TestingDatabaseTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

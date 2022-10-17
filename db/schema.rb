7dd2bb7752d66095bcd5858ed3b28c5f75c374c4
Corey Beavers <coreybeavers1999@gmail.com>
2022-10-17 19:14:17 -0400

    Added icon and added migration for table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_16_215621) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_230350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_16_215621) do
     t.bigint "weapon_category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["weapon_category_id"], name: "index_weapons_on_weapon_category_id"
   end
 

212cea99f5a18450f8ca46fb4e1162b06a92de3c
Corey Beavers <coreybeavers1999@gmail.com>
2022-10-17 20:21:40 -0400

    Fixed weapon ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -3,7 +3,7 @@ class RankingController < ApplicationController
     @weapons = Weapon.select('name,
        weapon_category_id,
        ((select count(id) from weapon_purchases where weapon_id = weapons.id)) as purchases,
-       ((select sum(damage) from weapon_damages where weapon_id = weapons.id)) as damage')
+       case when ((select sum(damage) from weapon_damages where weapon_id = weapons.id) is not null) then (select sum(damage) from weapon_damages where weapon_id = weapons.id) else 0 end as damage')
                      .order(Arel.sql('purchases desc, damage desc'))
 
   end

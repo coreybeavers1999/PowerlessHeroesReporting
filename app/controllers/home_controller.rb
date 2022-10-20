fded2b60754b5a31a9323aa66c75d77db4132394
Corey Beavers <coreybeavers1999@gmail.com>
2022-10-19 22:19:02 -0400

    Added game sessions to front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @game_list = GameSession.all
+
+    # Session List
+    sql = 'select map, substr(((updated_at - created_at)::time)::text,1,8) as time from game_sessions where active is true'
+    @game_list = ActiveRecord::Base.connection.exec_query(sql).rows
+
+    # Weapon List
     @weapon_list = Weapon.all
+
   end
 end
